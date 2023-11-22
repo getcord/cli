ec43972f1930849ad11ce24b132da41b3c8f20de
Maylynn <maylynn@cord.com>
2023-11-22 10:48:53 +0100

    cli: catch fetching of env variables when checking version
    We removed a catch from the util to get the env variables to the instances of it's uses. This one was missed - oops!!
    
    
    Test Plan:
    Remove .cord file, try to run cord - shouldn't see an error
    
    
    https://github.com/getcord/monorepo/assets/62358728/6b64aa81-410a-4e72-b27b-2ba2b4f0a1c5
    
    Reviewers: jwatzman, jacksongabbard, flooey
    
    Reviewed By: flooey
    
    Pull Request: https://github.com/getcord/monorepo/pull/7363
    
    monorepo-commit: 9c39a43cae4b5bae61b0a7fc4d5306443d8a3811

--- src/checkVersion.ts
+++ src/checkVersion.ts
@@ -1,15 +1,17 @@
 import chalk from 'chalk';
 import Box from 'cli-box';
+import fetch from 'node-fetch';
 import { getEnvVariables, updateEnvVariables } from 'src/utils';
-import { fetchCordRESTApi } from 'src/fetchCordRESTApi';
 import packageData from 'package.json';
 
 export async function checkVersion() {
-  const { VERSION_LAST_CHECKED } = await getEnvVariables();
+  const variables = await getEnvVariables().catch(() => {
+    /*no-op. probably just doesn't exist yet*/
+  });
 
   // we shouldn't fetch version or print update message if the version has been fetched within the last day
-  if (VERSION_LAST_CHECKED) {
-    const lastValidVersionDate = new Date(+VERSION_LAST_CHECKED);
+  if (variables?.VERSION_LAST_CHECKED) {
+    const lastValidVersionDate = new Date(+variables.VERSION_LAST_CHECKED);
     lastValidVersionDate.setDate(lastValidVersionDate.getDate() + 1);
     if (lastValidVersionDate.getTime() > Date.now()) {
       return;
@@ -19,8 +21,9 @@ export async function checkVersion() {
   // fetch latest version from npm
   // update env variables
   // if outdated, print update message
-  const res = await fetchCordRESTApi<{ version: string }>('cli-version');
-  const publishedVersion: string = res.version;
+  const res = await fetch('https://api.cord.com/v1/cli-version');
+  const response = (await res.json()) as { version: string };
+  const publishedVersion: string = response.version;
   await updateEnvVariables({
     VERSION_LAST_CHECKED: Date.now().toString(),
   });
